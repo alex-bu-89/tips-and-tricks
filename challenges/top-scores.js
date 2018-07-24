1b7ebff5b05768cfd5326fc5bc66646ef8b0c6c7
alex-bu-89 <alex.bu.89@gmail.com>
2018-07-24 15:10:24 +0200

    add top score challenge

--- challenges/top-scores.js
+++ challenges/top-scores.js
@@ -1,8 +1,32 @@
 const HIGHEST_POSSIBLE_SCORE = 100;
 let unsortedScores = [37, 89, 41, 65, 91, 53];
 
-function sortScores(unsortedArr, highestScore) {
-  
+function sortScores(unorderedScores, highestPossibleScore) {
+    // array of 0s at indices 0..highestPossibleScore
+    var scoreCounts = [];
+    for (var i = 0; i < highestPossibleScore + 1; i++) {
+        scoreCounts.push(0);
+    }
+
+    // populate scoreCounts
+    unorderedScores.forEach(function(score) {
+        scoreCounts[score]++;
+    });
+
+    // populate the final sorted array
+    var sortedScores = [];
+
+    // for each item in scoreCounts
+    for (var score = highestPossibleScore; score >= 0; score--) {
+        var count = scoreCounts[score];
+
+        // for the number of times the item occurs
+        for (var time = 0; time < count; time++) {
+            sortedScores.push(score);
+        }
+    }
+
+    return sortedScores;
 }
 
-sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE);
+console.log(sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE));
