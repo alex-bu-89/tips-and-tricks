04e096c780a1db86ac45aeda8f7b18d078f09e12
Alex Buyanov <alex.bu.89@gmail.com>
2019-08-22 14:51:35 +0200

    Update leftRotation.js

--- algorithms/leftRotation.js
+++ algorithms/leftRotation.js
@@ -4,14 +4,15 @@
  * For example [1,2,3,4,5] => [5,1,2,3,4]
  */
 function rotateArrayLeft(array, shiftAmount) {
-    var newArray = new Array(array.length);
+    const newArray = new Array(array.length);
+    
     for(var i = 0; i < array.length; i++){
-      var newLocation = (i + shiftAmount) % array.length;
+      const newLocation = (i + shiftAmount) % array.length;
       newArray[i] = array[newLocation];
     }
 
     return newArray.join(' ');
 }
 
-var arr = rotateArrayLeft([1,2,3,4,5], 4);
+const arr = rotateArrayLeft([1,2,3,4,5], 4);
 console.log(arr);
