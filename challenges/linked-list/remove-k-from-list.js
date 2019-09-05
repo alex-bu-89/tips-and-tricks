3e158a7e5e67ded36d7ef694dfaf4382cd11dc21
Alex Buyanov <alex.bu.89@gmail.com>
2019-09-05 18:00:51 +0200

    Add challenge

---
+++ challenges/linked-list/remove-k-from-list.js
@@ -0,0 +1,15 @@
+/**
+* Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+* 
+* Singly-linked lists are already defined with this interface:
+* function ListNode(x) {
+*   this.value = x;
+*   this.next = null;
+* }
+
+* @param {ListNode} l - ListNode
+* @param {int} n - value to remove
+* @return {return} new ListNode
+*/
+function removeKFromList(l, k) {
+}
