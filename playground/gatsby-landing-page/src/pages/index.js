1fd72ce0d37d4876c1bee70512a7081612b1f8c2
Alex Buyanov <alex.bu.89@gmail.com>
2020-02-06 22:30:38 +0100

    WIP gatsby-landing-page

--- playground/gatsby-landing-page/src/pages/index.js
+++ playground/gatsby-landing-page/src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </>
+  )
+}
