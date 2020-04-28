2072a481efe699cca4276a0da5ecfae1a0b92f09
Alex Buyanov <buyanov15@gmail.com>
2020-04-28 21:16:47 +0200

    Working on run-length-encoding compression

---
+++ challenges/javascript/run-length-encoding/run-length-encoding.ts
@@ -0,0 +1,26 @@
+export function encode(input: string): string {
+    // (.) - matches any char
+    // \1+ - matches one or more
+    const regexp = /(.)\1+/g;
+
+    const result = input.replace(regexp, (match, ...args) => {
+        return `${match.length}${args[0]}`;
+    });
+
+    return result;
+}
+
+export function decode(input: string): string {
+    // d+ - matches one or more decimal
+    // [a-zA-Z] - matches one letter
+    const regexp = /\d+[a-zA-Z]/g;
+
+    const result = input.replace(regexp, (match) => {
+        const [count, char] = match.split(/([\d\.]+)(.*)/).filter(r => r !== '')
+        return char.repeat(parseInt(count));
+    });
+
+    return result;
+}
+
+decode('4AF3BCF10FCD')
