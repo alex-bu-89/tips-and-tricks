028cbadbbf65bec58807d1a5ea4c4298b276691f
Alex Buyanov <buyanov15@gmail.com>
2020-06-23 14:24:29 +0200

    Move playground to own repo (#5)

--- playground/modal/vuejs/src/index.js
+++
@@ -1,4 +0,0 @@
-import VueSelect from './components/Modal.vue'
-
-export default VueSelect
-export { VueSelect }
--- playground/player-archive/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { profileReducer, aviabilityReducer } from './playerArchive';
-
-export default combineReducers({ 
-  playerProfile: profileReducer,
-  playerAviability: aviabilityReducer,
-});
